7b5d7ac52c160235ef345296d3a3758237975cd3
Teo <teodormihai.baila@ulbsibiu.ro>
2025-10-21 17:57:02 +0300

    Modified the url for backend.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const API_BASE = import.meta.env.VITE_API_URL || "";
-
+export const API_BASE =
+  import.meta.env.VITE_API_URL?.trim() ||
+  "https://backend-hwarang-new.onrender.com"; // fallback corect pt producție
