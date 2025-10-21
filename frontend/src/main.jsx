d9932074bc5d1f89688965bf5576542895a751e5
Teo <teodormihai.baila@ulbsibiu.ro>
2025-10-21 14:24:02 +0300

    Changed back to old working.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,10 @@ import App from './App';
 import './styles/theme.css';
 import './styles/mobile_ovverrides.css';
 
-import { AuthProvider } from './auth/AuthProvider';
-import ErrorBoundary from './components/ErrorBoundary';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <AuthProvider>
+    <React.StrictMode>
         <App/>
-      </AuthProvider>
-    </ErrorBoundary>
-  </React.StrictMode>
+    </React.StrictMode>
 );
