443af8a5f688e2b82ebb21b292e66e28d0070b59
Teo <teodormihai.baila@ulbsibiu.ro>
2025-11-03 20:00:27 +0200

    Tried for mobile app.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,21 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
+    plugins: [react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: true,
+            workbox: {navigateFallback: '/index.html'}
+        })],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+            },
+        },
     },
-  },
 })
